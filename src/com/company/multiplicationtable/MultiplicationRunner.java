5670566b7c0d8bb34bf07ebbee9cade543ccb1f8
Veera Prakash <veera.p0820@gmail.com>
2021-06-16 13:07:30 -0500

    method overloading example

--- src/com/company/multiplicationtable/MultiplicationRunner.java
+++ src/com/company/multiplicationtable/MultiplicationRunner.java
@@ -4,5 +4,6 @@ public class MultiplicationRunner {
     public static void main(String[] args) {
         MultiplicationTable table = new MultiplicationTable();
         table.multiply(5);
+        table.multiply(5, 0 ,20);
     }
 }
