8268397dfda4bdf1d817ca1d92a605c123a44a3a
Veera Prakash <veera.p0820@gmail.com>
2021-06-16 13:02:40 -0500

    print on new line

--- src/com/company/multiplicationtable/MultiplicationTable.java
+++ src/com/company/multiplicationtable/MultiplicationTable.java
@@ -3,7 +3,8 @@ package com.company.multiplicationtable;
 public class MultiplicationTable {
     void multiply(int multiplicationTable) {
         for(int i=0; i< 10; i++) {
-            System.out.printf("%d * %d = %d", multiplicationTable, i, multiplicationTable * i);
+            // print on new line
+            System.out.printf("%d * %d = %d", multiplicationTable, i, multiplicationTable * i).println();
         }
     }
 }
