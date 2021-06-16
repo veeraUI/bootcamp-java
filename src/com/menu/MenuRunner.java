99bc2e05e5150ce474ef60fb08b2765882a038a0
Veera Prakash <veera.p0820@gmail.com>
2021-06-16 17:54:10 -0500

    while loop with question to continue or stop

--- src/com/menu/MenuRunner.java
+++ src/com/menu/MenuRunner.java
@@ -6,35 +6,30 @@ public class MenuRunner {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Menu availableMenuOptions =  new Menu();
+        System.out.print("Would like to Start: Yes or No: ");
+        String start = input.next();
+        String answerYes = "Yes";
+        while (answerYes.equalsIgnoreCase(start)) {
 
-        System.out.print("Enter Number1: ");
-        int num1 = input.nextInt();
-        System.out.print("Enter Number2: ");
-        int num2 = input.nextInt();
-        availableMenuOptions.printOptions();
-        int option = input.nextInt();
+            System.out.print("Enter Number1: ");
+            int num1 = input.nextInt();
+            System.out.print("Enter Number2: ");
+            int num2 = input.nextInt();
+            availableMenuOptions.printOptions();
+            int option = input.nextInt();
 
-        int total = 0;
+            int total = switch (option) {
+                case 1 -> availableMenuOptions.add(num1, num2);
+                case 2 -> availableMenuOptions.subtract(num1, num2);
+                case 3 -> availableMenuOptions.divide(num1, num2);
+                case 4 -> availableMenuOptions.multiply(num1, num2);
+                default -> 0;
+            };
 
-        switch (option) {
-            case 1:
-                total = availableMenuOptions.add(num1, num2);
-                break;
-            case 2:
-                total = availableMenuOptions.subtract(num1, num2);
-                break;
-            case 3:
-                total = availableMenuOptions.divide(num1, num2);
-                break;
-            case 4:
-                total = availableMenuOptions.multiply(num1, num2);
-                break;
-            default:
-                total = 0;
+            System.out.printf("Here are the options you choose\nNumber1:%d \nNumber2: %d\nChoice: %d \nResult: %d ", num1, num2, option, total);
+            System.out.println("would you like start over? Yes or No: ");
+            start = input.next();
         }
-
-        System.out.printf("Here are the options you choose\nNumber1:%d \nNumber2: %d\nChoice: %d \nResult: %d ", num1, num2, option, total);
-
 
     }
 }
