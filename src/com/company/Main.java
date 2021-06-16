29e34b79048ad1990470a386d6f434fe19900b3c
Veera Prakash <veera.p0820@gmail.com>
2021-06-16 12:58:14 -0500

    first java program

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    static void helloWorld () {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+	    helloWorld();
+    }
+}
